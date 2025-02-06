f902e0c1842fae436b9f1a6520d26b969c6b03de
seivenbell <sbalaniu@uwaterloo.ca>
2025-02-05 21:31:38 -0500

    one more commit test

--- train_gpt2.py
+++ train_gpt2.py
@@ -11,6 +11,16 @@ from torch.nn import functional as F
 
 
 class GPTConfig:
+    """
+    GPTConfig is a configuration class for setting up the parameters of a GPT-2 model.
+
+    Attributes:
+        block_size (int): The size of each block in the model. Default is 256.
+        vocab_size (int): The size of the vocabulary. Default is 65.
+        n_layers (int): The number of layers in the model. Default is 6.
+        n_head (int): The number of attention heads in each layer. Default is 6.
+        n_mod (int): The dimension of the model. Default is 384.
+    """
     # Strong Typing in Python here
     block_size: int = 256
     vocab_size: int = 65
