c0af84e24b17faa18cbf1d59df5d07d0a54aa40d
Artem <adolhyi@uwaterloo.ca>
2025-02-07 23:18:37 -0500

    Test of the commit/removed coments

--- train_gpt2.py
+++ train_gpt2.py
@@ -9,16 +9,6 @@ from torch.nn import functional as F
 @dataclass 
 
 class GPTConfig:
-    """
-    Configuration class for the GPT model.
-
-    Attributes:
-        block_size (int): The size of each block in the model.
-        vocab_size (int): The size of the vocabulary.
-        n_layers (int): The number of layers in the model.
-        n_head (int): The number of attention heads.
-        n_mod (int): The dimension of the model.
-    """
     block_size: int = 256
     vocab_size: int = 65
     n_layers: int = 6
